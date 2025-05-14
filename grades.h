e6ef5af528b8f11f6e5070c8f2b626ef88902e04
Ramon Baez <ramonbaez1358@gmail.com>
2025-05-13 21:55:35 -0400

    Update grades.h

--- grades.h
+++ grades.h
@@ -5,9 +5,36 @@
 #include <vector>
 
 
-class grades{
+class Student{
+private:
+std::string id;
+std::string stduentName;
+std::vector<int>grades;
 public:
-vector<std::string>grades(null);
+Student(std::string studentID, std::string name, std::vector<int>studentGrades){
+    name = stduentName;
+    id = studentID;
+    grades = studentGrades;
+    std::cout << "Constructer called for student " << name << std::endl;
+
+}
+    {
+    ~ Student(){
+        std::cout << "destrcutor called for student: " << name << std::endl;
+    }
+
+
+    void Student::printGrades(){
+        std::cout << "Enter the number of ghrades you need to enter for student: " << std::endl;
+        int num = 0;
+        cin >> num;
+        std::vector<int>grades(num);
+        for(int i = 0; i < grades.size(); i ++){
+
+            std::cout << name << " grades where: " << i+1 << ".)" << grades.at(i) << "." << std::endl;
+        }
+
+    }
 
 }
 
